463e23820c7083ddf336159ab98df1f3703737fc
Niklas Klein <mail@taig.io>
2015-10-23 13:21:58 +0200

    Add support for error messages

---
+++ src/main/java/android/support/design/widget/ScopeStealing.java
@@ -0,0 +1,7 @@
+package android.support.design.widget;
+
+import android.view.animation.Interpolator;
+
+public class ScopeStealing extends AnimationUtils {
+    public static Interpolator INTERPOLATOR = FAST_OUT_SLOW_IN_INTERPOLATOR;
+}
